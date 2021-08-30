4f0d6ed4f92f229d31d5a466a58c3ba08d3d6098
dawsonsewell <dsewell@uwo.ca>
2021-08-30 16:05:49 -0400

    added new functions

--- higherOrderFunctions.hs
+++ higherOrderFunctions.hs
@@ -14,3 +14,17 @@ divideByTen = (/10)
 -- a funciton that checks if a supplied character is an upper case letter 
 isUpperAlphanum :: Char -> Bool 
 isUpperAlphanum = (`elem` ['A'..'Z'])
+
+
+applyTwice :: (a -> a) -> a -> a 
+applyTwice f x = f (f x) 
+
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = [] 
+zipWith' _ _ [] = [] 
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f y x = f x y 
