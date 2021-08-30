7f54f4910cf0cb814ef6bacabf0115d1044a45fd
dawsonsewell <dsewell@uwo.ca>
2021-08-30 16:17:50 -0400

    map and filter functions

--- higherOrderFunctions.hs
+++ higherOrderFunctions.hs
@@ -28,3 +28,15 @@ zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
 flip' :: (a -> b -> c) -> b -> a -> c
 flip' f y x = f x y 
+
+
+map :: (a -> b) -> [a] -> [b]
+map _ [] = [] 
+map f (x:xs) = f x : map f fx
+
+
+filter :: (a -> Bool) -> [a] -> [a]
+filter _ [] = [] 
+filter p (x:xs)
+    | p x       = x : filter p xs 
+    | otherwise = filter p xs 
--- recursionLH.hs
+++ recursionLH.hs
@@ -59,8 +59,8 @@ quicksort :: (Ord a) => [a] -> [a]
 
 quicksort [] = [] 
 quicksort (x:xs) = 
-    let smallerSorted = quicksort [a | a <- xs, a <= x]
-        biggerSorted = quicksort [a | a <- xs, a > x]
+    let smallerSorted = quicksort (filter (<=x) xs) -- OR use a list comprehension -> [a | a <- xs, a <= x]
+        biggerSorted = quicksort (filter (>x) xs) -- OR use a list comprehension -> [a | a <- xs, a > x]
     in smallerSorted ++ [x] ++ biggerSorted
 
 
