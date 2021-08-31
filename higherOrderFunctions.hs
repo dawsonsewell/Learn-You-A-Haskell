66d89ba4fd95a7fa4b674de05cb0d228e05c49c6
dawsonsewell <dsewell@uwo.ca>
2021-08-30 20:01:56 -0400

    Learned about left and right folds

--- higherOrderFunctions.hs
+++ higherOrderFunctions.hs
@@ -33,6 +33,9 @@ flip' f y x = f x y
 map :: (a -> b) -> [a] -> [b]
 map _ [] = [] 
 map f (x:xs) = f x : map f fx
+-- implement the map function with a right fold 
+map' (a -> b) -> [a] -> [b]
+map ' f xs = foldr (\x acc -> f x : acc) [] xs
 
 
 filter :: (a -> Bool) -> [a] -> [a]
@@ -40,3 +43,59 @@ filter _ [] = []
 filter p (x:xs)
     | p x       = x : filter p xs 
     | otherwise = filter p xs 
+
+
+
+largestDivisible :: (integral a) => a 
+largestDivisible = head (filter p [100000, 99999..])
+    where p x = x `mod` 3829 == 0 
+
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n = n:chain (n `div` 2) 
+    | odd n = n:chain (n*3 + 1)
+
+
+-- too run the code below you need to comment out above lines EXCEPT for `chain`
+
+numLongChains :: Int 
+--numLongChains = length (filter isLong (map chain [1..100]))
+    --where isLong xs = length xs > 15 
+-- we can write the above code more precisely by using a lambda "\"
+--numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+
+-- the sum function, but using a fold instead of explicit recursion 
+sum' :: (Num a) => [a] -> a 
+-- sum' xs = foldl (\acc x -> acc + x) 0 xs
+-- the lambda function (\acc x -> acc + x) is the same as (+)
+-- b/c functions are curried in Haskell, we can re-write the function as follows 
+sum' = foldl (+) 0 
+
+
+-- Implement `elem` with a left fold 
+elem' :: (Eq a) => a -> [a] -> Bool 
+elem' y ys = foldl (\acc x -> if x ==y then True else acc) False ys 
+
+
+-- implement a bunch of standard library functins by using folds 
+maximum' :: (Ord a) => [a] -> a 
+maximum' = foldr1 (\x acc -> if x > acc then x else acc)
+
+reverse' :: [a] -> [a] 
+reverse' = foldl (\acc c -> x : acc) [] 
+
+product' :: (Num a) => [a] -> a 
+product' = foldr1 (*)
+
+filter' :: (a -> Bool) -> [a] -> [a] 
+filter' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+head' :: [a] -> a 
+head' = foldr1 (\x _ -> x)
+
+last' :: [a] -> a 
+last' = foldl1 (\_ -> x)
